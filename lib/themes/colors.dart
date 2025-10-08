f2ff1849e4a3737f5aa115320f8b17b7b4cce279
alessandrosaddo <a.saddo@studenti.unimol.it>
2025-10-08 13:32:37 +0200

    add lightBorderColor and lightWidgetBackground in AppColors

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // Tema Chiaro
-  static const lightBackground = Color(0xFFF6E8DB);
+  static const lightBackground = Color(0xFFFBFBFB);
   static const lightPrimary = Color(0xFF047CDB);
   static const lightSecondary = Color(0xFFFAF3E9);
   static const lightText = Color(0xFF393838);
+  static const lightBorderColor = Color(0xFFBDD4E7);
+  static const lightWidgetBackground = Color(0xFFF6F3EB);
 
   // Tema Scuro
   static const darkBackground = Color(0xFF0F0F0F);
