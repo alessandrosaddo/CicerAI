5a73a2f78cdd08047845b40ea7cb9c5f57efd0fb
alessandrosaddo <a.saddo@studenti.unimol.it>
2025-11-05 20:32:21 +0100

    Create Integration test and add insert city

---
+++ integration_test/app_test.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_places_flutter/google_places_flutter.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:cicer_ai/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('TappaWidget Integration Test', () {
+    testWidgets('Compilazione completa di una tappa con tutti i campi', (
+      WidgetTester tester,
+    ) async {
+      //================= STEP 1 Avvio dell'app ==============================
+      app.main();
+      await tester.pumpAndSettle();
+      debugPrint('✅ App avviata con successo');
+
+      // Verifica presenza testo nella schermata iniziale
+      expect(find.text('Personalizza il tuo Viaggio'), findsOneWidget);
+      debugPrint('✅ Home Screen caricata');
+
+      //================= STEP 2 Inserimento città ==============================
+
+      // Verifica Ricerca per l'itinerario
+      expect(
+        find.widgetWithText(
+          GooglePlaceAutoCompleteTextField,
+          'Cerca Città/Rileva Posizione',
+        ),
+        findsOneWidget,
+      );
+      debugPrint('✅ Campo città trovato');
+
+      // Inserisci 'Roma' nella ricerca
+      final cityField = find.byType(GooglePlaceAutoCompleteTextField);
+      await tester.tap(cityField);
+      await tester.pumpAndSettle();
+
+      await tester.enterText(cityField, 'Roma');
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pumpAndSettle(const Duration(seconds: 4));
+      debugPrint('✅ Digitato "Roma" nel campo città...');
+
+      // Trova tutti i suggerimenti dell’autocomplete
+      final suggestions = find.byType(ListTile);
+      expect(suggestions, findsWidgets);
+      debugPrint('✅ Suggerimenti trovati: ${suggestions.evaluate().length}');
+
+      //Seleziona il secondo risultato della lista dall'autocomplete
+      await tester.tap(suggestions.at(1));
+      await tester.pumpAndSettle(const Duration(seconds: 2));
+      debugPrint('✅ Selezionato il secondo risultato dell’autocomplete');
+
+      await tester.pump(const Duration(seconds: 3));
+      debugPrint('⏳ Attendo ricostruzione del widget dopo selezione città...');
+
+
+    });
+  });
+}
